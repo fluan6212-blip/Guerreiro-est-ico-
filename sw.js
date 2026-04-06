ba87a8b6290dd881dc054c1a565afac33b05c3f8
fluan6212-blip <fluan6212@gmail.com>
2026-04-05 21:05:17 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+// Este é o Escudo Offline do Software Ferreira
+const CACHE_NAME = 'ferreira-v1';
+const assets = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
